36903898e1eee3fbb01235f7cdbe86a197b4821a
Tim-Smart <tim@fostle.com>
2010-09-06 02:19:32 +1200

    Remove old directory walker

--- build/utils.js
+++ build/utils.js
@@ -1,4 +1,4 @@
-var DirectoryWalker, Task, crypto, directoryWalker, fs, path;
+var DirectoryWalker, Task, crypto, fs, path;
 fs = require('fs');
 path = require('path');
 crypto = require('crypto');
@@ -62,23 +62,3 @@ DirectoryWalker.prototype.onDir = function(dir) {
   });
 };
 exports.DirectoryWalker = DirectoryWalker;
-directoryWalker = (exports.directoryWalker = function(dir, callback, maxLevels, currentLevel, fromRoot) {
-  maxLevels = 'number' === typeof maxLevels ? maxLevels : 0;
-  currentLevel = 'number' === typeof currentLevel ? currentLevel : 1;
-  fromRoot = 'string' === typeof fromRoot ? fromRoot : '';
-  return fs.readdir(dir, function(error, files) {
-    return error ? console.log(error.message) : files.forEach(function(file) {
-      return fs.stat(path.join(dir, file), function(error, stats) {
-        if (error) {
-          return console.log(error.message);
-        }
-        if (stats.isDirectory()) {
-          if (0 === maxLevels || maxLevels > currentLevel) {
-            directoryWalker(path.join(dir, file), callback, maxLevels, 1 + currentLevel, fromRoot + file + '/');
-          }
-        }
-        return callback.call(stats, file, fromRoot, path.join(dir, file), stats);
-      });
-    });
-  });
-});
